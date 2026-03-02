ed0655bc94233ccfb1ca1b4a72bd6da976bd4bba
SupeChicken666 <me@supechicken666.dev>
2026-03-02 11:49:18 +0000

    Refactor directory structure, add initial A16 releases
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- append.rb
+++ append.rb
@@ -4,32 +4,47 @@ require 'digest/sha2'
 
 ASB_LEVEL = ARGV[0]
 
-%w[system vendor].each do |img|
-  %w[arm64 x86_64].each do |arch|
-    %w[GAPPS VANILLA MAINLINE].each do |type|
-      next unless File.exist?("#{img}/waydroid_tv_#{arch}/#{type}.json")
+%w[A13 A13TV A14 A14TV A15 A15TV A16 A16TV].each do |variant|
+  %w[system vendor].each do |img|
+    %w[arm64 x86_64].each do |arch|
+      %w[GAPPS VANILLA MAINLINE].each do |type|
+        json_path   = File.join(variant, img == 'system' ? 'system/lineage' : img, "waydroid_#{arch}/#{type}.json")
+        target_name = "#{variant.end_with?('TV') ? 'waydroid_tv' : 'waydroid'}_#{arch}"
+        lineage_ver = \
+            case variant
+              when /^A13/
+                "lineage-20.0"
+              when /^A14/
+                "lineage-21.0"
+              when /^A15/
+                "lineage-22.2"
+              when /^A16/
+                "lineage-23.2"
+              end
 
-      json = JSON.load_file("#{img}/waydroid_tv_#{arch}/#{type}.json", symbolize_names: true)
+        next unless File.exist?(json_path)
+        json = JSON.load_file(json_path, symbolize_names: true)
 
-      Dir["lineage-*-#{type}-waydroid_tv_#{arch}-#{img}.zip"].each do |zip|
-        stat = File.stat(zip)
+        Dir["#{lineage_ver}-*-#{type}-#{target_name}-#{img}.zip"].each do |zip|
+          stat = File.stat(zip)
 
-        json[:response].delete_if { |e| e[:filename] == zip }
-        json[:response] << {
-          datetime: stat.mtime.to_i,
-          filename: zip,
-          id:       Digest::SHA256.file(zip),
-          romtype:  type,
-          asb:      ASB_LEVEL,
-          size:     stat.size,
-          url:      "https://sourceforge.net/projects/waydroid-atv/files/images/#{img}/waydroid_tv_#{arch}/#{zip}/download",
-          version:  zip[/^lineage-(.+?)-/, 1]
-        }
+          json[:response].delete_if { |e| e[:filename] == zip }
+          json[:response] << {
+            datetime: stat.mtime.to_i,
+            filename: zip,
+            id:       Digest::SHA256.file(zip),
+            romtype:  type,
+            asb:      ASB_LEVEL,
+            size:     stat.size,
+            url:      "https://sourceforge.net/projects/waydroid-atv/files/images/#{img}/#{target_name}/#{zip}/download",
+            version:  lineage_ver.delete_prefix('lineage-')
+          }
+        end
+
+        json[:response].sort_by! { |e| - e[:datetime] }
+
+        File.write(json_path, JSON.pretty_generate(json))
       end
-
-      json[:response].sort_by! { |e| - e[:datetime] }
-
-      File.write("#{img}/waydroid_tv_#{arch}/#{type}.json", JSON.pretty_generate(json))
     end
   end
 end
--- index.js
+++ index.js
@@ -9,21 +9,25 @@ const AVAILABLE_IMG_TYPES = [
   ['vendor', 'x86_64', 'MAINLINE']
 ];
 
-async function getLatestRelease(img, arch, type) {
-  const update_channel = await fetch(`/${img}/waydroid_tv_${arch}/${type}.json`).then(r => r.json());
+const FRIENDLY_ARCH_NAME = { arm64: 'ARM64', x86_64: 'x86-64' },
+      channelSelector    = document.querySelector('#channel-selector > select');
+
+async function getLatestRelease(img, arch, type, variant) {
+  const update_channel = await fetch(`/${variant}/${img == 'system' ? 'system/lineage' : img}/waydroid_${arch}/${type}.json`).then(r => r.json());
   return update_channel.response[0];
 }
 
-async function updateTable(img, arch, type) {
+async function updateTable(img, arch, type, variant) {
   const table          = document.getElementById(`${img}-images`),
         img_table_elem = document.createElement('tr');
 
   table.appendChild(img_table_elem);
 
-  const img_info = await getLatestRelease(img, arch, type);
+  const img_info = await getLatestRelease(img, arch, type, variant);
 
   img_table_elem.innerHTML = `
-    <td>${arch}</td>
+    <td>${FRIENDLY_ARCH_NAME[arch]}</td>
+    <td>LineageOS ${img_info.version}</td>
     <td>${type}</td>
     <td>${new Date(img_info.datetime * 1000).toISOString().split('T')[0]}</td>
     <td>${img_info.asb}</td>
@@ -31,4 +35,15 @@ async function updateTable(img, arch, type) {
   `;
 }
 
-window.onload = () => { for (types of AVAILABLE_IMG_TYPES) updateTable(...types); };
+function onChannelUpdate() {
+  const tables = document.querySelectorAll(`.list > table`);
+
+  tables[0].innerHTML = tables[0].innerHTML.replace(/(?<=\<\/tbody\>).*/s, '');
+  tables[1].innerHTML = tables[1].innerHTML.replace(/(?<=\<\/tbody\>).*/s, '');
+
+  for (type of AVAILABLE_IMG_TYPES) {
+    updateTable(...type, channelSelector.value);
+  }
+}
+
+channelSelector.onchange = window.onload = () => onChannelUpdate();
